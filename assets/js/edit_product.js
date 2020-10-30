61c5fa5d15b9a5e4675e6105602b8eeedbf55126
Antonio De Falco <antonio.defalco@minick.de>
2020-10-30 15:44:39 +0100

    handling disabled property with js

--- assets/js/edit_product.js
+++ assets/js/edit_product.js
@@ -12,9 +12,10 @@ document.addEventListener('DOMContentLoaded', function(event) {
         jQuery(`[id*=${id}]`).each(function(index, el) {
             index = el.id.replace(id, '');
 
-            jQuery(`#${id}${index}`).on('change', function(e) {
+            jQuery(`#${id}${index}`).one('change', function(e) {
                 var disable = jQuery(this).val() === 'issue_new_license';
                 jQuery(`#lmfwc_subscription_renewal_reset_action${index}`).prop('disabled', disable);
+                jQuery(`#lmfwc_subscription_cost_per_activation_action${index}`).prop('disabled', disable);
                 jQuery(`#lmfwc_subscription_renewal_interval_type${index}`).prop('disabled', disable);
                 jQuery(`#lmfwc_subscription_renewal_custom_interval${index}`).prop('disabled', disable);
                 jQuery(`#lmfwc_subscription_renewal_custom_period${index}`).prop('disabled', disable);
