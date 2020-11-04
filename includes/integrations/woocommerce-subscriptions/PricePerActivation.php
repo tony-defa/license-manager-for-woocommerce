9cfc5f8b876b7fd1d7feebd6b740c3238923b3d8
Antonio De Falco <antonio.defalco@minick.de>
2020-11-04 11:32:12 +0100

    improved string formatting and some refactoring

--- includes/integrations/woocommerce-subscriptions/PricePerActivation.php
+++ includes/integrations/woocommerce-subscriptions/PricePerActivation.php
@@ -50,23 +50,18 @@ class PricePerActivation
 		$trialLength        = WC_Subscriptions_Product::get_trial_length( $product );
         $trialPeriod        = WC_Subscriptions_Product::get_trial_period( $product );
 
-        $d = 'license-manager-for-woocommerce';
-
         $price = $include['price'];
-        $sub = $include['subscription_price']
-                    ? sprintf(__('%s / activation', $d), $price, wcs_get_subscription_period_strings($billingInterval, $billingPeriod)) 
-                    : '';
-        $period = $include['subscription_period'] 
-                    ? sprintf(_n('%s, billed each %s', '%s, billed every %s', $billingInterval, $d), $sub, wcs_get_subscription_period_strings($billingInterval, $billingPeriod)) 
+        $period = $include['subscription_price'] && $include['subscription_period'] 
+                    ? sprintf(_nx('%s / activation, billed each %s', '%s / activation, billed every %s', $billingInterval, 'Contains price per activation and billing period', 'license-manager-for-woocommerce'), $price, wcs_get_subscription_period_strings($billingInterval, $billingPeriod)) 
                     : '';
         $length = $include['subscription_length'] && $subscriptionLength != 0 
-                    ? sprintf(_n('for a %s', 'for %s', $subscriptionLength, $d), wcs_get_subscription_period_strings($subscriptionLength, $billingPeriod)) 
+                    ? sprintf(_nx('for a %s', 'for %s', $subscriptionLength, 'The length of the subscription', 'license-manager-for-woocommerce'), wcs_get_subscription_period_strings($subscriptionLength, $billingPeriod)) 
                     : '';
         $trial = $include['trial_length'] && $trialLength != 0 
-                    ? sprintf(_n('with %s %s free trial', 'with a %s-%s free trial', $trialLength, $d), $trialLength, $trialPeriod) 
+                    ? sprintf(_nx('with %s %s free trial', 'with a %s-%s free trial', $trialLength, 'Describes the length of the trail period of the subscription', 'license-manager-for-woocommerce'), $trialLength, $trialPeriod) 
                     : '';
         $fee = $include['sign_up_fee'] && $signUpFee != 0 
-                    ? sprintf(__('and a %s sign-up fee', $d),  wc_price($signUpFee)) 
+                    ? sprintf(_x('and a %s sign-up fee', 'The sign up fee pricing', 'license-manager-for-woocommerce'),  wc_price($signUpFee)) 
                     : '';
 
         $subscriptionString = sprintf('%s %s %s %s', $period, $length, $trial, $fee);
@@ -77,23 +72,70 @@ class PricePerActivation
 
     function maybeCreateSubscriptionPriceString($subscriptionString, $subscriptionDetails) 
     {
-        if (!isset($subscriptionDetails['use_cost_per_activation']) || $subscriptionDetails['use_cost_per_activation'] !== true) {
-            return $subscriptionString;
-        }
-
         $amount = $subscriptionDetails['recurring_amount'];
         $interval = $subscriptionDetails['subscription_interval'];
         $period = $subscriptionDetails['subscription_period'];
         $length = $subscriptionDetails['subscription_length'];
+        $trialLength = $subscriptionDetails['trial_length'];
 
-        $d = 'license-manager-for-woocommerce';
+        if (isset($subscriptionDetails['use_cost_per_activation']) && $subscriptionDetails['use_cost_per_activation'] === false) {
+            return $subscriptionString;
+        } else if (!isset($subscriptionDetails['use_cost_per_activation'])) {
 
-        $interval = sprintf(_n('/ activation, billed each %s', '/ activation, billed every %s', $interval, $d), wcs_get_subscription_period_strings($interval, $period));
+            // After the order is completed and the order e-mail is being generated, the 
+            // 'woocommerce_cart_subscription_string_details' hook will not be executed, 
+            // so we have a hart time finding out if the current price string should contain
+            // a 'cost_per_activation' price string. We can try to find out by getting all 
+            // product ids that match the current subscription details and will continue with 
+            // our price string formatting, only if we have exactly one matching product
+            // or product variation. This is not an ideal solution as this can not always
+            // guarantees a save match. 
+            // In the edit subscription admin panel this can also fail, if the product meta 
+            // data has changed since the subscription was ordered or if the data does not 
+            // match the subscription details anymore.
+            $allIds = get_posts(array(
+                'post_type' => array('product', 'product_variation'),
+                'post_status' => 'publish',
+                'numberposts' => -1,
+                'fields' => 'ids',
+                'meta_query' => array(
+                    array('key' => 'lmfwc_subscription_cost_per_activation_action',
+                          'value' => 'cost_per_activation', 
+                          'compare' => '=',
+                    ),
+                    array('key' => '_subscription_period_interval',
+                          'value' => $interval, 
+                          'compare' => '=',
+                    ),
+                    array('key' => '_subscription_period',
+                          'value' => $period, 
+                          'compare' => '=',
+                    )
+                ),
+            ));
+
+            $removeList = array();
+            foreach ($allIds as $id) {
+                $price = wc_get_product($id)->get_price();
+                if (floatval($amount) !== floatval($price)) {
+                    $removeList[] = $id;
+                }
+            }
+            $allIds = array_diff($allIds, $removeList);
+
+            // return the current formatted string if there are none or multiple ids 
+            // that match the subscription details
+            if (count($allIds) !== 1) {
+                return $subscriptionString;
+            }
+        }
+
+        $interval = sprintf(_nx('%s / activation, billed each %s', '%s / activation, billed every %s', $interval, 'Contains price per activation and billing period', 'license-manager-for-woocommerce'), $amount, wcs_get_subscription_period_strings($interval, $period));
         $length = $length != 0 
-                    ? sprintf(_n('for a %s', 'for %s', $length, $d), wcs_get_subscription_period_strings($length, $period)) 
+                    ? sprintf(_nx('for a %s', 'for %s', $length, 'The length of the subscription', 'license-manager-for-woocommerce'), wcs_get_subscription_period_strings($length, $period)) 
                     : '';
 
-        $subscriptionString = sprintf('%s %s %s', $amount, $interval, $length);
+        $subscriptionString = sprintf('%s %s', $interval, $length);
         $subscriptionString = trim(str_replace('  ', ' ', $subscriptionString));
 
         return $subscriptionString;
@@ -144,10 +186,11 @@ class PricePerActivation
         /** @var int $parentOrderId */
         $parentOrderId = $subscription->get_parent_id();
 
-        /** @var false|WC_Order $parentOrder */
-        $parentOrder = wc_get_order($parentOrderId);
-
         $items = $newOrder->get_items();
+        if (!$items) {
+            error_log("LMFWC: Skipped order #{$newOrder->get_id()} because no items are contained.");
+        }
+        
         foreach ($items as $item) {
 
             /** @var int $productId */
@@ -163,7 +206,7 @@ class PricePerActivation
             /** @var false|LicenseResourceModel[] $licenses */
             $licenses = lmfwc_get_licenses(
                 array(
-                    'order_id' => $parentOrder->get_id(),
+                    'order_id' => $parentOrderId,
                     'product_id' => $productId
                 )
             );
@@ -199,6 +242,10 @@ class PricePerActivation
 
         // TODO: figure out what to do with orders where the total is below the minimum excepted expenditure but not 0 € (e.g. stripe minimum 0.50 €)
         if ($newOrder->get_total() > 0) {
+            
+            /** @var false|WC_Order $parentOrder */
+            $parentOrder = wc_get_order($parentOrderId);
+
             $gateway = $parentOrder->get_payment_method();
             switch ($gateway) {
                 case 'stripe':
